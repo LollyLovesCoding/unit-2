60b749ce2e98678915b4358c8b9bf04bb960146d
Lilyana Zhang <impro4everrr@gmail.com>
2025-09-15 10:40:46 -0400

    Create 2.2-tracing-numerical-variables.py

---
+++ 2.2-tracing-numerical-variables.py
@@ -0,0 +1,27 @@
+# 1 Variables and division vs 
+# integer (floor) division
+a = 4
+b = 1
+c = 1 / 4 = 0.25
+d = 4 // 1 = 0
+
+# 2 Variables and incrementing
+a = 5
+a = 5 + 2 = 7
+b = 7
+a = 7 + 1 = 8
+b = 7 + 8 = 15
+
+# 3 Variables and increment operators
+a = 8
+a += 3 = 11
+b = 11
+a += 2 = 13
+b += a = 24
+
+# 4 Variables and increment operator 2
+total = 0
+total += 1 = 1
+total += 1 = 2
+total += 1 = 3
+total += 1 = 4
