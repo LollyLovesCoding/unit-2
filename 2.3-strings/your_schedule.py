2ad55470c58407f567236a47a19f8aef2d188a47
Lilyana Zhang <impro4everrr@gmail.com>
2025-09-16 10:39:20 -0400

    Create your_schedule.py

---
+++ 2.3-strings/your_schedule.py
@@ -0,0 +1,15 @@
+course_1 = "Mathematics"
+teacher_1 = "Ms. Do"
+course_2 = "Computer Science"
+teacher_2 = "Mr. Gallo"
+course_3 = "Science"
+teacher_3 = "Mr. Niro"
+course_4 = "English"
+teacher_4 = "Ms. Laviolette"
+
+print("+" + "-" * 43 + "+")
+print(f"| 1 |{course_1:>18} |{teacher_1:>18} |")
+print(f"| 2 |{course_2:>18} |{teacher_2:>18} |")
+print(f"| 3 |{course_3:>18} |{teacher_3:>18} |")
+print(f"| 4 |{course_4:>18} |{teacher_4:>18} |")
+print("+" + "-" * 43 + "+")
