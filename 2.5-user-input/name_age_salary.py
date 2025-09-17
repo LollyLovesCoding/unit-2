cf4e2af529653ec8cace6b7750ddff90817edd95
Lilyana Zhang <impro4everrr@gmail.com>
2025-09-17 10:17:38 -0400

    Create name_age_salary.py

---
+++ 2.5-user-input/name_age_salary.py
@@ -0,0 +1,4 @@
+name = str(input("Hello. What is your name? "))
+age = int(input(f"Hi, {name}! How old are you? "))
+salary = float(input(f"So you're {age}, eh?  That's not old at all!\nHow much do you make, {name}? "))
+print(f"{salary}! I hope that's per hour and not per year! LOL!")
