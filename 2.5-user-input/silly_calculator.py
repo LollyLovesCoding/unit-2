48be6ed5c76b085f3a53b50368cf889c49dcd7da
Lilyana Zhang <impro4everrr@gmail.com>
2025-09-17 10:28:14 -0400

    Create silly_calculator.py

---
+++ 2.5-user-input/silly_calculator.py
@@ -0,0 +1,5 @@
+first_num = float(input("What is your first number? "))
+second_num = float(input("What is your second number? "))
+third_num = float(input("What is your third number? "))
+
+print(f"( {first_num} + {second_num} + {third_num} ) / 2 is... {(first_num + second_num + third_num)/2}")
